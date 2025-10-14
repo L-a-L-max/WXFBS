ad0500dbeb41062d165ed99bca9d445efd8f38ce
Obvious <15553435+zhupy123@user.noreply.gitee.com>
2025-10-14 10:44:19 +0800

    智能体问题修复（20251014）

--- cube-framework/src/main/java/com/cube/framework/config/properties/PermitAllUrlProperties.java
+++ cube-framework/src/main/java/com/cube/framework/config/properties/PermitAllUrlProperties.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.RegExUtils;
 import org.springframework.beans.BeansException;
@@ -16,6 +17,7 @@ import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.util.pattern.PathPattern;
 import com.cube.common.annotation.Anonymous;
 
 /**
@@ -34,6 +36,28 @@ public class PermitAllUrlProperties implements InitializingBean, ApplicationCont
 
     public String ASTERISK = "*";
 
+    /**
+     * 获取RequestMappingInfo中的URL模式
+     * 兼容Spring Boot 3.x版本的API变化
+     */
+    private Set<String> getUrlPatterns(RequestMappingInfo info) {
+        // 尝试使用传统的getPatternsCondition()方法
+        if (info.getPatternsCondition() != null) {
+            return info.getPatternsCondition().getPatterns();
+        }
+        
+        // 对于Spring Boot 3.x，尝试使用getPathPatternsCondition()
+        if (info.getPathPatternsCondition() != null) {
+            Set<PathPattern> pathPatterns = info.getPathPatternsCondition().getPatterns();
+            return pathPatterns.stream()
+                    .map(PathPattern::getPatternString)
+                    .collect(java.util.stream.Collectors.toSet());
+        }
+        
+        // 如果都获取不到，返回空集合
+        return java.util.Collections.emptySet();
+    }
+
     @Override
     public void afterPropertiesSet()
     {
@@ -42,16 +66,17 @@ public class PermitAllUrlProperties implements InitializingBean, ApplicationCont
 
         map.keySet().forEach(info -> {
             HandlerMethod handlerMethod = map.get(info);
+            Set<String> patterns = getUrlPatterns(info);
 
             // 获取方法上边的注解 替代path variable 为 *
             Anonymous method = AnnotationUtils.findAnnotation(handlerMethod.getMethod(), Anonymous.class);
-            Optional.ofNullable(method).ifPresent(anonymous -> Objects.requireNonNull(info.getPatternsCondition().getPatterns())
-                    .forEach(url -> urls.add(RegExUtils.replaceAll(url, PATTERN, ASTERISK))));
+            Optional.ofNullable(method).ifPresent(anonymous -> 
+                patterns.forEach(url -> urls.add(RegExUtils.replaceAll(url, PATTERN, ASTERISK))));
 
             // 获取类上边的注解, 替代path variable 为 *
             Anonymous controller = AnnotationUtils.findAnnotation(handlerMethod.getBeanType(), Anonymous.class);
-            Optional.ofNullable(controller).ifPresent(anonymous -> Objects.requireNonNull(info.getPatternsCondition().getPatterns())
-                    .forEach(url -> urls.add(RegExUtils.replaceAll(url, PATTERN, ASTERISK))));
+            Optional.ofNullable(controller).ifPresent(anonymous -> 
+                patterns.forEach(url -> urls.add(RegExUtils.replaceAll(url, PATTERN, ASTERISK))));
         });
     }
 
